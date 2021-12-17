976bd30c8731fc64e82e5c787a2a75cd642a0eee
Pp0y <llkk695@gmail.com>
2021-12-17 14:39:49 +0700

    add new sourcecode and add .gitignore

---
+++ Cal2.py
@@ -0,0 +1,12 @@
+x = input("x = ")
+
+if float(x) >=80:
+    print("A")
+elif float(x) >=70:
+    printf("B")
+elif float(x) >=60:
+    printf("C")
+elif float(x) >=50:
+    printf("D")
+else :
+    printf("F")
